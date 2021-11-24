aefb904f0a325c79c4e86d1415c3cff6d3f2a231
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2021-11-24 19:16:23 +0700

    Update Highscore.vue

--- src/components/Highscore.vue
+++ src/components/Highscore.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="min-h-screen bg-white bg-no-repeat bg-cover" :style="{'background-image': `url(${require('../assets/home.png')})`}">
-        <h2 v-text="'Highscore'" />
+        <div class="flex ml-6">
+            <button @click="home" class="button h-12 w-12 rounded-full mx-4 mt-6">
+                <i class="mdi mdi-chevron-left mdi-36px" />
+            </button>
+        </div>
+        <div class="flex justify-center font-bold mt-6">
+            <h2 class="text-green-500 text-4xl text-center w-72" v-text="'Skor Terbaik'" />
+        </div>
+
+        <div class="flex font-bold mt-6 md:justify-center ml-6 md:ml-0">
+            <ol class="text-left list-decimal">
+                <li class="text-lg text-green-500" v-for="(score, i) in scores" :key="i" v-text="`(${score.score}) - ${score.name}`" />
+            </ol>
+        </div>
     </div>
 </template>
 
@@ -8,6 +21,18 @@
 
 export default {
     name: 'Highscore',
-    emits: ['moving']
+    emits: ['moving'],
+    data(){
+        return {
+            scores: [
+                {name: 'Testing', score: 100}
+            ]
+        }
+    },
+    methods: {
+        home(){
+            this.$emit('moving', 1)
+        },
+    }
 }
 </script>
