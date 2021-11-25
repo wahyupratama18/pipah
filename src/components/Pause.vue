03de1af692686ddb10c07003c231fafef72acf95
reowicaksono <reo.wicaksono.2005356@gmail.com>
2021-11-25 17:06:00 +0700

    added pause menu

--- src/components/Pause.vue
+++ src/components/Pause.vue
@@ -1,47 +1,25 @@
 <template>
-    <div class="min-h-screen bg-white bg-no-repeat bg-cover" :style="{'background-image': `url(${require('../assets/home.png')})`}">
-        <div class="flex ml-6">
-            <button @click="home" class="button h-12 w-12 rounded-full mx-4 mt-6">
-                <i class="mdi mdi-chevron-left mdi-36px" />
-            </button>
+    <div class="min-h-screen bg-white bg-no-repeat bg-cover" :style="{'background-image': `url(${require('../assets/game.jpg')})`}">
+        <div class="flex">
         </div>
-        <div class="flex justify-center font-bold mt-6">
-            <h2 class="text-green-500 text-4xl text-center w-72" v-text="'Skor Terbaik'" />
+        <div class="flex justify-center mt-16"  :style="{'position': 'absolute','z-index': '2','width': '100%','padding': '50px'}">
+            <img src="../assets/Pause.png" alt="Pause" width ="400" height="400">
         </div>
 
-        <div class="flex font-bold mt-6 md:justify-center ml-6 md:ml-0">
-            <ol class="text-left list-decimal" v-if="scores.length > 0">
-                <li class="text-lg text-green-500" v-for="(score, i) in scores" :key="i" v-text="`(${score.score}) - ${score.name}`" />
-            </ol>
-            <h4 v-else v-text="'Data skor tidak tersedia, yuk main'" class="text-lg text-green-500" />
-        </div>
+            <div class="flex justify-center " :style="{'position': 'absolute','z-index': '3','width': '100%','margin-top': '250px'}">
+                <button @click="play" class="button font-bold px-6 text-xl" v-text="'P L A Y '"/>
+            </div>
+            <div class="flex justify-center" :style="{'position': 'absolute','z-index': '3','width': '100%','margin-top': '340px'}">
+                <button @click="play" class="button font-bold px-6 text-xl" v-text="'E X I T '"/>
+            </div>
     </div>
 </template>
 
 <script>
-
 export default {
-    name: 'Highscore',
-    emits: ['moving'],
-    data(){
-        return {
-            scores: [
-                {name: 'Testing', score: 100},
-                {name: 'Testing', score: 150},
-                {name: 'Testing', score: 75}, 
-            ]
-        }
-    },
-    methods: {
-        home(){
-            this.$emit('moving', 1)
-        },
-    },
-    mounted() {
-        if (localStorage.getItem('scores')) {
-            this.scores = JSON.parse(localStorage.getItem('scores'))
-            .sort((a,b) => b.score - a.score).slice(0, 5)
-        }
-    }
+    name: 'Play',
+    emits: ['moving']
 }
 </script>
+
+
