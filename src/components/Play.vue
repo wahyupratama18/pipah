2e1a99cca90e98b13d91852e1aea5d8794372101
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2021-11-25 13:43:55 +0700

    Update gambar

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="min-h-screen bg-white bg-no-repeat bg-cover" :style="{'background-image': `url(${require('../assets/game.jpg')})`}">
-        <div class="text-green-500" v-text="'Test Play'" />
+        <div class="flex">
+            <button @click="viewScore" class="button h-12 w-12 rounded-full ml-6 mt-6">
+                <i class="mdi mdi-pause mdi-36px" />
+            </button>
+        </div>
+
+        <div class="flex mt-64 ml-12">
+            <img src="../assets/anorganik.png" alt="Tempat Sampah" class="h-48">
+            <img src="../assets/organik.png" alt="Tempat Sampah" class="h-48">
+        </div>
     </div>
 </template>
 
 <script>
-
 export default {
     name: 'Play',
     emits: ['moving']
