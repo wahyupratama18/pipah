3ce869cbecf029fd1ede7cbbe88e1d265ab8ecb0
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2021-11-28 22:04:21 +0700

    GL compliant

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1281858.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 61493961, 61494320 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import BaseGames from './BaseGames.vue'
-import './assets/tailwind.css'
-
-createApp(BaseGames).mount('#app')
